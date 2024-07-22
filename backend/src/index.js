048169f56f99eefcbefcc428e9eea010881a9138
jaccovk <s1158428@student.windesheim.nl>
2024-07-22 20:19:04 +0200

    updated bootstrap with test email sender

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,6 +19,15 @@ module.exports = {
     try {
       const result = await strapi.entityService.findMany("sections.hero")
       console.log(result)
+
+      const email = await strapi.plugins.email.services.email.send({
+        to: "jaccovankooten@hotmail.nl",
+        from: "laccovankooten@gmail.com",
+        subject: 'The Strapi Email plugin worked successfully',
+        text: 'This is the email body.',
+        html: '<p>This is the email html.</p>',
+      })
+      console.log("email", email)
     } catch (error) {
       console.log(error);
     }
