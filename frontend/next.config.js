22cc43b7aa82b1fec908d8adf3e89f9979c219ba
jaccovk <s1158428@student.windesheim.nl>
2024-07-26 19:32:09 +0200

    updated next.config with eslint skip on build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,25 +1,27 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '1339',
-        pathname: '/uploads/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "1339",
+        pathname: "/uploads/**",
       },
       {
-
-        protocol: 'https',
-        hostname: '**.test.jaccovankooten.nl',
-        pathname: '/uploads/**',
+        protocol: "https",
+        hostname: "**.test.jaccovankooten.nl",
+        pathname: "/uploads/**",
       },
       {
-        protocol: 'https',
-        hostname: 'strapi.**',
-        pathname: '/uploads/**',
-      }
+        protocol: "https",
+        hostname: "strapi.**",
+        pathname: "/uploads/**",
+      },
     ],
   },
 }
