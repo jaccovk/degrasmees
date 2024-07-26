1b064f60c6dd10ef5683aa2a99c88776e4a101d8
jaccovk <s1158428@student.windesheim.nl>
2024-07-26 18:41:40 +0200

    setted up a first email template with test info

--- backend/src/api/submission/content-types/submission/lifecycles.js
+++ backend/src/api/submission/content-types/submission/lifecycles.js
@@ -1,12 +1,23 @@
 module.exports = {
   async afterUpdate(event) {
     console.log("event >>>>", event)
+    const emailFrom = process.env.MAIL_DEFAULT_FROM
+    if (!emailFrom) {
+      console.error("MAIL_DEFAULT_FROM is not set")
+      return
+    }
+
     try {
+      const { email, name } = event.result
+      if (!email) {
+        console.error("Email is not set")
+        return
+      }
+
       const result = await strapi.plugins.email.services.email.send({
-        to: "Test" + " <" + "jaccovankooten@hotmail.nl" + ">",
-        from: 'laccovankooten@gmail.com', //e.g. single sender verification in SendGrid
-        subject: 'The Strapi Email plugin worked successfully',
-        text: 'Hello world!',
+        to: name ? name + " <" + email + ">" : email,
+        from: emailFrom,
+        subject: 'The Strapi Email plugin worked successfully!',
         html: 'Hello world!',
       })
 
